e7a0e3836dda601de2b1d421ce7df03d70b68d1d
Antonijo Galic <antonijodev@gmail.com>
2024-03-17 00:10:35 +0100

    Adding block styles to the functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,4 @@
 <?php
-add_action( 'after_setup_theme', 'twiske_theme_setup' );
-
 function twiske_theme_setup() {
     // Add support for block styles
 	add_theme_support( 'wp-block-styles' );
@@ -8,11 +6,15 @@ function twiske_theme_setup() {
     // Enqueue editor styles
     add_editor_style( 'styles.css' );
 }
+add_action( 'after_setup_theme', 'twiske_theme_setup' );
 
 /*-------------------------------------------------------------
 Enqueue Styles
 --------------------------------------------------------------*/
+
 function twiske_styles() {
     // Register Stylesheet
     wp_enqueue_style('twiske-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'));
+    wp_enqueue_style('twiske-style-blocks', get_template_directory_uri() . '/assets/css/blocks.css');
 }
+add_action( 'wp_enqueue_scripts', 'twiske_styles' );
