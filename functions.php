8bb4b2ec11d7b7c8d4ef78a66277eca835a4c61f
Antonijo Galic <antonijodev@gmail.com>
2024-04-10 00:58:31 +0200

    Implementing particle animation on home page

--- functions.php
+++ functions.php
@@ -18,3 +18,29 @@ function twiske_styles() {
     wp_enqueue_style('twiske-style-blocks', get_template_directory_uri() . '/assets/css/blocks.css');
 }
 add_action( 'wp_enqueue_scripts', 'twiske_styles' );
+
+/*-------------------------------------------------------------
+Enqueue Scripts
+--------------------------------------------------------------*/
+
+function twiske_theme_particles() {
+	wp_enqueue_script( 
+		'twiske-theme-particles',
+		get_parent_theme_file_uri( '/assets/js/particles.js' ),
+		array(),
+		wp_get_theme()->get( 'Version' ),
+		true
+	);
+}
+add_action( 'wp_enqueue_scripts', 'twiske_theme_particles' );
+
+function twiske_theme_app_particles() {
+	wp_enqueue_script( 
+		'twiske-theme-app-particles',
+		get_parent_theme_file_uri( '/assets/js/appParticles.js' ),
+		array(),
+		wp_get_theme()->get( 'Version' ),
+		true
+	);
+}
+add_action( 'wp_enqueue_scripts', 'twiske_theme_app_particles' );
